5053f2fa39a3d9e1da54e73d48717d61eb7eb669
Giovanna <gparejas23@outlook.com>
2026-05-11 10:09:14 -0300

    feat: Adicionando o menu de forma responsiva no projeto

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,23 @@
-window.onload = function(){
+// Início: Loading
 
-    const intro=document.getElementById("inicio");
+window.onload = function () {
 
-    setTimeout(()=>{
+    const intro = document.getElementById("inicio");
+
+    setTimeout(() => {
         intro.classList.add("efeito-out")
-        setTimeout(()=>{
-            intro.style.display="none";
+        setTimeout(() => {
+            intro.style.display = "none";
             content.classList.remove("hidden");
             content.classList.add("efeito-in");
-        },1000)
-    },3000)
+        }, 1000)
+    }, 3000)
 }
 
-const inicio      = document.getElementById('inicio');
-const barraFill   = document.getElementById('barra-fill');
+const inicio = document.getElementById('inicio');
+const barraFill = document.getElementById('barra-fill');
 const porcentagem = document.getElementById('porcentagem');
+const header = document.querySelector('header'); 
 
 let progresso = 0;
 
@@ -32,6 +35,7 @@ const intervalo = setInterval(() => {
             inicio.style.opacity = '0';
             setTimeout(() => {
                 inicio.style.display = 'none';
+                header.classList.add('visivel'); 
             }, 1000);
         }, 400);
 
@@ -42,3 +46,17 @@ const intervalo = setInterval(() => {
     porcentagem.textContent = Math.round(progresso) + '%';
 
 }, 60);
+
+// Fim: Loading
+
+// Início: Menu
+
+const hamburguer = document.getElementById('hamburguer');
+const menu = document.getElementById('link-menu');
+
+hamburguer.addEventListener('click', () => {
+    menu.classList.toggle('aberto');
+    hamburguer.classList.toggle('aberto');
+});
+
+// Fim: Menu
